caaf870346cbc76f0c34cddd211e1a2167a7d7d9
Sven <sven.otter@student.nhlstenden.com>
2023-07-12 10:19:10 +0200

    - property binding (beginnetje gemaakt)

--- Pages/Admin/Uxos/Create.cshtml.cs
+++ Pages/Admin/Uxos/Create.cshtml.cs
@@ -1,11 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Mined.Models;
 
 namespace Mined.Pages.Admin.Uxos
 {
     public class CreateModel : PageModel
     {
-        public void OnGet()
+        public Uxo Uxo { get; set; }
+		public Payload Payload { get; set; }
+        public Category Category { get; set; }
+		public void OnGet()
         {
         }
     }
