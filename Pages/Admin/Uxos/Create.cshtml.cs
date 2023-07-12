a0f2e5e5aff85515ab4f17a435d8ce6092d30d9b
Sven <sven.otter@student.nhlstenden.com>
2023-07-12 22:57:25 +0200

    nog niet database probleem opgelost

--- Pages/Admin/Uxos/Create.cshtml.cs
+++ Pages/Admin/Uxos/Create.cshtml.cs
@@ -9,26 +9,28 @@ namespace Mined.Pages.Admin.Uxos
     public class CreateModel : PageModel
     {
         private readonly MinedDbContext _db;
-        public CreateModel(MinedDbContext db)
-        {
-            _db = db;
-        }
-        public Uxo Uxo { get; set; }
-		public Payload Payload { get; set; }
-        public Category Category { get; set; }
-        public UxoPayload UxoPayload { get; set; }
+
+		//      public Uxo Uxo { get; set; }
+		//public Payload Payload { get; set; }
+		[BindProperty]
+        public Category? Category { get; set; }
+		// public UxoPayload UxoPayload { get; set; }
+		public CreateModel(MinedDbContext db)
+		{
+			_db = db;
+		}
 
 		public void OnGet()
         {
         }
-		public async Task<IActionResult> OnPostAddUxo()
+		public async Task<IActionResult> OnPostAddUxo(Category category)
         {
 			if (ModelState.IsValid) 
             {
-				await _db.Uxos.AddAsync(Uxo);
-				await _db.Categories.AddAsync(Category);
-				await _db.Payloads.AddAsync(Payload);
-				await _db.UxoPayloads.AddAsync(UxoPayload);
+				//await _db.Uxos.AddAsync(Uxo);
+				await _db.Categories.AddAsync(category);
+				//await _db.Payloads.AddAsync(Payload);
+				//await _db.UxoPayloads.AddAsync(UxoPayload);
 				await _db.SaveChangesAsync();
 				return RedirectToPage("Index");
             }
