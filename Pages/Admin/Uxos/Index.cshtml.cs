e4eaa759e4082167327827c5c73c0f6ebaa853ed
Sven <sven.otter@student.nhlstenden.com>
2023-07-14 10:37:53 +0200

    Mined zonderpayloads tabellen

--- Pages/Admin/Uxos/Index.cshtml.cs
+++ Pages/Admin/Uxos/Index.cshtml.cs
@@ -8,9 +8,7 @@ namespace Mined.Pages.Admin.Uxos
     {
         private readonly MinedDbContext _db;
         public IEnumerable<Category> Categories { get; set; }
-        //public IEnumerable<Image> Images { get; set; }
-        //public IEnumerable<Payload> Payloads { get; set; }
-        //public IEnumerable<UxoPayload> UxoPayloads { get; set; }
+        public IEnumerable<Image> Images { get; set; }
         public IEnumerable<Uxo> Uxos { get; set; }
         public IndexModel(MinedDbContext db)
         {
@@ -19,9 +17,7 @@ namespace Mined.Pages.Admin.Uxos
         public void OnGet()
         {
             Categories = _db.Categories;
-            //Images = _db.Images;
-            //Payloads = _db.Payloads;
-            //UxoPayloads = _db.UxoPayloads;
+            Images = _db.Images;
             Uxos = _db.Uxos;
         }
     }
